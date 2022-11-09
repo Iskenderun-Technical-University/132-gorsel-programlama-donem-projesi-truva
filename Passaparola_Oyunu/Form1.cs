b5506b3ea83ec69ecae032fa725f0532744e0c19
YUSUF AKICI <98983922+YusufAkici@users.noreply.github.com>
2022-11-09 11:56:39 +0300

    yusuf akıcı

---
+++ Passaparola_Oyunu/Form1.cs
@@ -0,0 +1,70 @@
+namespace Passaparola_Oyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CEVAP_Enter(object sender, EventArgs e)
+        {
+
+        }
+        int soruno = 0, dogru = 0, yanlis = 0;
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                switch(soruno)
+                {
+                    case 1:
+                        if(textBox1.Text == "akdeniz")
+                        {
+                            button1.BackColor = Color.Green;
+                            dogru++;
+                            label2.Text = dogru.ToString();
+                        }
+                        else
+                        {
+                            button1.BackColor = Color.Red;
+                            yanlis++;
+                           
+                            label4.Text = yanlis.ToString();
+                        }
+                        break;
+                        
+                   
+
+
+                }
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            linkLabel1.Text = "Sonraki";
+            soruno++;
+            this.Text = soruno.ToString();
+
+            if (soruno == 1)
+            {
+                richTextBox1.Text = "Ülkemizin güney kýsmýndaki kýyý bölgeleri";
+                button1.BackColor = Color.Yellow;
+            }
+          
+
+        }
+    }
+}
