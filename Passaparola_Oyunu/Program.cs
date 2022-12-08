028e853f5287065489f3808a6027e05ed302f380
salihzengin <sz014747@gmail.com>
2022-12-08 11:25:39 +0300

    Update Program.cs

--- Passaparola_Oyunu/Program.cs
+++ Passaparola_Oyunu/Program.cs
@@ -8,7 +8,7 @@ namespace Passaparola_Oyunu
         [STAThread]
         static void Main()
         {
-            //deneme
+           
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
